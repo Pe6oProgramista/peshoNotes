807207f5fa6767179e1a2a0b8bb5f8e50df5d9f5
Pe6oProgramista <p.milev3@gmail.com>
2017-10-30 03:24:28 -0700

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  get 'posts/:id/send', to: 'posts#send'
+  get 'notes/api', to: 'posts#send'
   root 'posts#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
