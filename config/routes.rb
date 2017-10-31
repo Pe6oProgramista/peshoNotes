a52f59fc1a2cb1e7e2b8a52e7e7db315a6553fd8
Pe6oProgramista <p.milev3@gmail.com>
2017-10-31 13:35:18 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/notes/api', to: 'posts#send'
+  get '/notes/api', to: 'posts#new'
+  # match '/notes/api' => 'posts#new'
   root 'posts#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
