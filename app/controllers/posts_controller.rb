3817af31dcb9bb1dc94289a179de42313831390f
Pe6oProgramista <p.milev3@gmail.com>
2017-11-05 08:27:41 -0800

    dasda

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
       post = Post.create(content: params[:message])
       render json: {url: "https://pesho-notes.herokuapp.com/notes/api/" + post.id.to_s}
     elsif request.content_type =~ /form/
-      @post = Post.new(post_params)
+      @post = Post..new({content: params[:content]})
     
         if @post.save
           render "send", locals: { url: "https://pesho-notes.herokuapp.com/notes/api/" + @post.id.to_s}
